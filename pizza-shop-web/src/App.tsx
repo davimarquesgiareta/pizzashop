b5ca554c39a7d9bcd3bccda7432ae43f19ff6e58
Davi Marques <davimgcb@gmail.com>
2025-02-20 21:42:41 -0300

    feat: create router dom

--- pizza-shop-web/src/App.tsx
+++ pizza-shop-web/src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from "./components/ui/button";
+import { RouterProvider } from "react-router";
 import "./index.css";
+import { router } from "./routes";
 
 function App() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Button> clique aqui </Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ pizza-shop-web/src/pages/auth/sign-in.tsx
@@ -0,0 +1,3 @@
+export function SigIn() {
+  return <h1>Sigin</h1>;
+}
