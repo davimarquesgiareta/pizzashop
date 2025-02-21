b660cc893db4c7f2f3c2cd617a0debc4241dda9c
Davi Marques <davimgcb@gmail.com>
2025-02-20 21:53:48 -0300

    feat: application layouts with outlet

--- pizza-shop-web/src/routes.tsx
+++ pizza-shop-web/src/routes.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router";
 import { Dashboard } from "./pages/app/dashboard";
 import { SigIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
-  { path: "/sign-in", element: <SigIn /> },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "/", element: <Dashboard /> }],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [{ path: "/sign-in", element: <SigIn /> }],
+  },
 ]);
