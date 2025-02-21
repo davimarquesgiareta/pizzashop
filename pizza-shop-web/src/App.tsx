45027c6d5e16051e4517863f48cf70a84256d092
Davi Marques <davimgcb@gmail.com>
2025-02-20 21:26:21 -0300

    feat: rebuild project with shadcnui and tailwind css

---
+++ pizza-shop-web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button> clique aqui </Button>
+    </div>
+  );
+}
+
+export default App;
