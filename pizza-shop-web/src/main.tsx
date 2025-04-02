639865dc04b580f6447841f7ec85415118abe0a7
Davi Marques <davimgcb@gmail.com>
2025-04-02 14:58:10 -0300

    feat: msw setup

--- pizza-shop-web/src/main.tsx
+++ pizza-shop-web/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './app.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./app.tsx";
+import { enableMSW } from "./api/mocks/index.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
