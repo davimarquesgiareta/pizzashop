c66dcd896564e5d6a8e223daabf67687939ada4a
Davi Marques <davimgcb@gmail.com>
2025-04-07 12:02:20 -0300

    feat: updated profile e2e test

---
+++ pizza-shop-web/test/store-profile.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "@playwright/test";
+
+test("updated profile successfully", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  await page.getByRole("button", { name: "Pizza Shop" }).click();
+  await page.getByRole("menuitem", { name: "Perfil da loja" }).click();
+
+  await page.getByLabel("Nome").fill("Davi Pizza");
+  await page.getByLabel("Descrição").fill("Another Description");
+
+  await page.getByRole("button", { name: "Salvar" }).click();
+
+  await page.waitForLoadState("networkidle");
+
+  const toast = page.getByText("Perfil atualizado com sucesso!");
+
+  await expect(toast).toBeVisible();
+
+  await page.getByRole("button", { name: "Close" }).click();
+
+  await page.waitForTimeout(250);
+
+  expect(page.getByRole("button", { name: "Davi Pizza" })).toBeVisible();
+});
